04563741350b29159f0a90fdf3ab1f0657dd0916
brandon-schabel <brandonschabel1995@gmail.com>
2017-12-12 21:22:37 -0800

    Finish Section 3 of course

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -11,22 +11,45 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log("Was Clicked!");
     this.setState({
       persons: [
-        { name: 'Maxmilian', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Brandon', age: 21 },
         { name: 'Beanz', age: 1.4 }
       ]
     })
   }
 
+  nameChangedHandler = (event) => {
+    // console.log("Was Clicked!");
+    this.setState({
+      persons: [
+        { name: "Max", age: 28 },
+        // no matter which component you type into, the second name shoudl change */
+        { name: event.target.value, age: 21 },
+        { name: 'Beanz', age: 1.4 }
+      ]
+    })
+  }
+
   render() {
+    // styling using javascript instead of css directly (this is called inline styling)
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1x solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
         <h1> Hi, I'm a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        {/* Below an arrow function is used instead of binding it like we did below with one of the Person components*/}
+        <button 
+        style={style}
+        onClick={ () => this.switchNameHandler('Maximillian!!')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -34,6 +57,10 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+          // below is a reference to switchNameHandler Method
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          // below for changed we set a reference to the nameChangedHandler
+          changed={this.nameChangedHandler}
         >
           My hobbies are overReacting
         </Person>
--- first-app/src/Person/Person.js
+++ first-app/src/Person/Person.js
@@ -1,15 +1,21 @@
 import React from 'react'
-
+import Person from './Person.css'
 // this is a function based component, use function based whenever possible
 const person = props => {
   return (
-    <div>
-      <p>
+    // className refers to the css class, not a JS class
+    <div className="Person">
+      <p onClick={props.click}>
+        {' '}
+        {/* click is the name of the property that we will call */}
         I'm {props.name} and I am {props.age} years old
       </p>
       <p>{props.children}</p>{' '}
       {/* props.childrens is reserved for text we want to insert 
-                                    between creatin the component or other JS data*/}
+        between creatin the component or other JS data*/}
+      <input type="text" onChange={props.changed} value={props.name} />
+      {/* props.changed is the prop we will use to set the method that will change the name
+          and value={props.name} is binding to the value of this components name for 2-way data binding*/}
     </div>
   )
 }
